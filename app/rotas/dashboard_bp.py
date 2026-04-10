b41607fe473a050cd43ee903f8a2ede452b639d6
AppMotoristaPro <appmotoristapro@gmail.com>
2026-04-10 17:51:06 -0300

    a

--- app/rotas/dashboard_bp.py
+++ app/rotas/dashboard_bp.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+# app/rotas/dashboard_bp.py
+from flask import Blueprint, render_template, send_file
 from app.models.video import Video
+import os
 
 dashboard_bp = Blueprint('dashboard', __name__)
 
@@ -9,7 +11,6 @@ def index():
 
 @dashboard_bp.route('/video')
 def video(): 
-    # Para o front-end antigo funcionar, vamos mandar o último vídeo feito
     ultimo_video = Video.query.order_by(Video.criado_em.desc()).first()
     return render_template('dashboard/video.html', video=ultimo_video)
 
@@ -17,4 +18,18 @@ def video():
 def historico():
     videos = Video.query.order_by(Video.criado_em.desc()).all()
     return render_template('dashboard/historico.html', videos=videos)
+
+@dashboard_bp.route('/stream')
+def stream_video():
+    caminho = os.path.abspath("reels_final.mp4")
+    if os.path.exists(caminho):
+        return send_file(caminho, mimetype="video/mp4")
+    return "Vídeo não encontrado no servidor.", 404
+
+@dashboard_bp.route('/download')
+def download_video():
+    caminho = os.path.abspath("reels_final.mp4")
+    if os.path.exists(caminho):
+        return send_file(caminho, as_attachment=True)
+    return "Vídeo não encontrado no servidor.", 404
 
